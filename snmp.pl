68b505ca14b9e426b6dc9395a06d7715431b3c36
natedumm21 <nrd400@francis.edu>
2024-01-22 19:51:50 -0500

    fixed ' to

--- snmp.pl
+++ snmp.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 #print"5\n" 
-my $sudo_sessions = 'journalctl _COMM=sudo | grep -c "COMMAND=sh"';
+my $sudo_sessions = `journalctl _COMM=sudo | grep -c "COMMAND=sh"`;
 print $sudosessions; 
 #testsave
+
+
