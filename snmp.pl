e35c6970daec48346fbd9d80856dac28975366f0
natedumm21 <nrd400@francis.edu>
2024-01-22 20:18:15 -0500

    Updated after a few attempts with 18 as number

--- snmp.pl
+++ snmp.pl
@@ -4,8 +4,8 @@ use strict;
 use warnings;
 
 #print"5\n" 
-my $sudo_sessions = `journalctl _COMM=sudo | grep -c "COMMAND=sh"`;
-print $sudosessions; 
+my $sudo_sessions = `journalctl | grep "sudo" | wc -l`;
+print $sudo_sessions; 
 #testsave
 
 
