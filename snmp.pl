b44ce49b7b055308c8cc6695e8f8e3ba23da7f93
natedumm21 <nrd400@francis.edu>
2024-01-24 09:29:27 -0500

    added to search

--- snmp.pl
+++ snmp.pl
@@ -4,9 +4,7 @@ use strict;
 use warnings;
 
 #print"5\n" 
-my $sudo_sessions = `journalctl | grep -c "sudo:session"`;
+my $sudo_sessions = `journalctl | grep -c "pam_unix(sudo:session)"`;
 chomp $sudo_sessions; 
 print $sudo_sessions;
 #testsave
-#test
-
