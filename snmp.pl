effbb4676c481f922df86b5b3f5e691da1d30e7b
natedumm21 <nrd400@francis.edu>
2024-01-25 12:51:22 -0500

    output edit

--- snmp.pl
+++ snmp.pl
@@ -6,5 +6,6 @@ use warnings;
 #print"5\n" 
 my $sudo_sessions = `journalctl | grep -c "pam_unix(sudo:session): session opened"`;
 chomp $sudo_sessions; 
-print $sudo_sessions;
+print ".1.3.6.1.2.1.1 = \ "$sudo_sessions\"";
+print "5555555555555555555555"; 
 #testsave
